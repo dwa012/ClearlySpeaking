736f965deabe9a8c775b837ba1f88bb1601be24d
Daniel Ward <dwa012@gmail.com>
2013-05-13 11:32:15 -0500

    finally was able to have the app engine server change the slide on the page.

--- chrome_ext/js/eventPage.js
+++ chrome_ext/js/eventPage.js
@@ -1,31 +1,10 @@
 var tokenURL = "https://clearly-speaking.appspot.com/token"
+var channel;
+var channelPorts = [];
 
-function response(request, sender, sendResponse) {
-
-    console.log(request.action);
-
-    if (request.action == "connect_action"){
-        console.log("got connect action message");
-        getToken();
-        sendResponse({});
-    } else if (request.action == "show_action"){
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.pageAction.show(tabs[0].id);
-        });
-
-        sendResponse({});
-    } else {
-        console.log("event: " + request.action);
-        console.log("event: first");
-    }
-
-}
-
-var handler =
-{
+var handler = {
     onopen: function () {
-       console.log("opened");
+        console.log("opened");
     },
     onerror: function (error) {
         console.log("error: " + error.description);
@@ -33,29 +12,68 @@ var handler =
     onclose: function () {
         console.log("opened");
     },
-    onmessage: function (evt) {
-
-        console.log("got a message: " + evt.data);
-
-        chrome.tabs.getSelected(null, function(tab) {
-            chrome.tabs.sendMessage(tab.id, {action: "advance_slide"}, function(response) {
-                //left blank
-            });
-        });
-    }
+    onmessage: onMessage
 };
+
+// this is the response method for the one time connections
+function response(request, sender, sendResponse) {
+
+    // if the content script says that it is ok to show the action.
+    if (request.action == "show_action") {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.pageAction.show(tabs[0].id);
+        });
+
+        sendResponse({});
+    }
+}
+
+
+// this is the on message handler for the channel to the app engine server
+function onMessage(event) {
+    // Move to the next slide when we get a message from the app engine server
+    var notification = {action: "move_that_bus"};
+    for (var i = 0, len = channelPorts.length; i < len; i++) {
+        channelPorts[i].postMessage(notification);
+    }
+}
+
 
 function getToken() {
     console.log("fetching the token")
-    $.getJSON(tokenURL, function(data) {
+    $.getJSON(tokenURL,function (data) {
+        channel = new goog.appengine.Channel(data["token"]);
+        channel.open(handler);
 
-        var token = data["token"];
-        console.log(data);
-
-        channel = new goog.appengine.Channel(token);
-        socket = channel.open(handler);
-
-    }).error(function(error) { console.log("error: " + error) });
+    }).error(function (error) {
+            console.log("error: " + error)
+        });
 }
 
+
 chrome.runtime.onMessage.addListener(response);
+
+
+// a tab requests connection to the background script
+chrome.extension.onConnect.addListener(function (port) {
+    var tabId = port.sender.tab.id;
+    console.log('Received request from content script', port);
+
+    port.onMessage.addListener(function (msg) {
+        // setup the channel to the app engine server
+        if (msg.action == "connect_action") {
+            getToken();
+        }
+    });
+
+    // add the connected port to the list of open ports
+    if (channelPorts.indexOf(port) == -1) {
+        channelPorts.push(port);
+    }
+
+    // when a port disconnects, remove it from the list
+    port.onDisconnect.addListener(function () {
+        channelPorts.splice(channelPorts.indexOf(tabId), 1);
+    }); // end of onDisconnect Listener
+
+}); // end of onConnect Listener
